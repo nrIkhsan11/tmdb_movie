ce81b2e665ca9f489d8b4401c6e92fc8d96d9f34
ikhsan <nriksan2001@gmail.com>
2024-02-06 20:27:29 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/nurikhsan/tmdb_movie/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nurikhsan.tmdb_movie
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/appconstant.dart
@@ -0,0 +1,8 @@
+class AppConstant{
+
+  static const String baseUrl = 'https://api.themoviedb.org/3/';
+  static const String apiKey = '4f7d4f956853bf6fc0a3ae1fb03fc929';
+  static const String urlImage780 = 'https://image.tmdb.org/t/p/w780';
+  static const String urlImageOriginal = 'https://image.tmdb.org/t/p/original';
+
+}
